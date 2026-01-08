0286a729563e0ca2ce1cba77b5490ab98e543599
Namlamron <namlamron@gmail.com>
2026-01-08 08:56:36 -0800

    Autoupdating start script

--- Start.bat
+++ Start.bat
@@ -7,6 +7,15 @@ echo ========================================
 echo     McManager - Starting Server
 echo ========================================
 echo.
+
+echo Checking for updates...
+git pull
+echo.
+
+echo Checking dependencies...
+call npm install
+echo.
+
 echo Web Interface: http://localhost:3000
 echo.
 echo Press Ctrl+C to stop the server
